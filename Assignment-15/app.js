514c387df5b4578b6dae04d144c66c819fa3c516
Anas Raza <ar470684@gmail.com>
2026-08-22 19:36:59 +0500

    assignment-14 till 6

---
+++ Assignment-15/app.js
@@ -0,0 +1,62 @@
+// CH. 35-38
+// // 1.
+// function displayDateAndTime() {
+//     const date = new Date();
+//     document.write(date);
+// }
+// displayDateAndTime();
+
+// // 2.
+// function greet(firstName, lastName) {
+//     const fullName = firstName + " " + lastName;
+//     alert("Hello, " + fullName);
+// }
+// greet("Anas", "Raza");
+
+// // 3.
+// const num1 = +prompt("Enter num one");
+// const num2 = +prompt("Enter num two");
+// function sum(num1, num2) {
+//     const totalSum = num1 + num2;
+//     return totalSum;
+// }
+// alert("Sum of two numbers: " + sum(num1, num2))
+
+// // 4.
+// function calculate(num1, num2, opr) {
+//     if (opr == "+") {
+//         return num1 + num2
+//     } else if (opr == "-") {
+//         return num1 - num2
+//     } else if (opr == "*") {
+//         return num1 * num2
+//     } else if (opr == "/") {
+//         return num1 / num2
+//     } else {
+//         return "Invalid operator"
+//     }
+// }
+// document.write("Result: " + calculate(10, 5, "/"));
+
+// // 5.
+// function square(num) {
+//     return num * num;
+// }
+// document.write(square(4));
+
+// // 6.
+// function factorial(num) {
+//     if (num == 1 || num == 0) {
+//         return 1;
+//     }
+
+//     let result = num * factorial(num - 1);
+//     return result;
+// }
+// document.write(factorial(5))
+
+// // 7.
+// function displayCounting(start, end) {
+//     document.w
+
+// }
